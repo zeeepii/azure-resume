79d0db102ee45f510e6c261fecded2b4ce1ff33b
zied guermazi <zied.gormazi@enis.tn>
2023-11-21 16:37:46 +0100

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -7,7 +7,7 @@ const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
+    fetch(localFunctionApi).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
